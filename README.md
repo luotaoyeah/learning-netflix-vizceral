4739e1d53afb282ec53e3763cac949ca7c7f12b2
luotao <luotaoyeah@gmail.com>
2019-03-06 10:00:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# learning-netflix-vizceral
+Learning [Netflix Vizceral Component](https://github.com/Netflix/vizceral)
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+/* globals __dirname process */
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // publicPath: '/',
+    filename: 'vizceral.[hash].bundle.js'
+  },
+  resolve: {
+    alias: {
+      react: path.resolve('node_modules/react'),
+    },
+    extensions: ['.jsx', '.js'],
+    modules: ['node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      { test: /\.woff2?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.otf$/, use: 'file-loader' },
+      { test: /\.ttf$/, use: 'file-loader' },
+      { test: /\.eot$/, use: 'file-loader' },
+      { test: /\.svg$/, use: 'file-loader' },
+      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.css$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }] }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // Automtically detect jQuery and $ as free var in modules
+      // and inject the jquery library
+      // This is required by many jquery plugins
+      jQuery: 'jquery',
+      $: 'jquery'
+    }),
+    new webpack.DefinePlugin({
+      __HIDE_DATA__: !!process.env.HIDE_DATA
+    }),
+    new HtmlWebpackPlugin({
+      title: 'learning-netflix-vizceral',
+      template: './src/index.html',
+      favicon: './src/favicon.ico',
+      inject: true
+    })
+  ]
+};
